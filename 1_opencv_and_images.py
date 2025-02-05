e600d6382336911de906f86cf302e722e97c7b4f
Christopher, Malili <malilichristopher@gmail.com>
2025-02-05 14:13:41 -0800

    Completed Question 1b

--- 1_opencv_and_images.py
+++ 1_opencv_and_images.py
@@ -9,9 +9,13 @@ import matplotlib.pyplot as plt
 import cv2
 #from PIL import Image
 
-path = "../../MachineVision1/Estado_de_Mexico.jpg"
+# Question 1a: Loading and displaying and image in opencv
+path = "Estado_de_Mexico.jpg"
 #image = Image.open()
 image = cv2.imread(path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.imshow(image)
 
+# Question 1b: Cropping an image
+crop = image[800:1000, 800:1000]
+plt.imshow(crop)
