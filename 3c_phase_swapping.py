432eb6a97d855b109b5ed3be8dd3df3c4f274afb
Christopher, Malili <malilichristopher@gmail.com>
2025-02-15 16:19:09 -0800

    Question 3c phase swapping implemented in rgb from gray

--- 3c_phase_swapping.py
+++ 3c_phase_swapping.py
@@ -14,64 +14,84 @@ path_woman1 = "woman1.jpeg"
 path_woman2 = "woman2.jpeg"
 
 # Reading in image of woman1 and plotting it
-img_woman1 = cv2.imread(path_woman1)
-img_woman1 = cv2.cvtColor(img_woman1, cv2.COLOR_BGR2GRAY)
-plt.imshow(img_woman1)
-plt.axis("off")
-plt.show()
+def read_and_display(path):
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
+    return img
 
+woman1 = read_and_display(path_woman1)
+woman2 = read_and_display(path_woman2)
+
+def compute_dft_gray(img):        
 # converting image of woman 1 to frequency domain and plotting magnitude and phase
-dft_woman1 = cv2.dft(np.float32(img_woman1), flags=cv2.DFT_COMPLEX_OUTPUT)
-dft_woman1_shifted = np.fft.fftshift(dft_woman1)
-magnitude_woman1, phase_woman1 = cv2.cartToPolar(dft_woman1_shifted[:,:,0],
-                                                 dft_woman1_shifted[:,:,1])
-magnitude_woman1_normalized = 100*np.log(magnitude_woman1)
-plt.imshow(magnitude_woman1_normalized)
+    dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
+    dft_shifted = np.fft.fftshift(dft)
+    magnitude, phase = cv2.cartToPolar(dft_shifted[:,:,0], dft_shifted[:,:,1])
+    return magnitude, phase
 
 
-phase_woman1_normalized = cv2.normalize(phase_woman1, None, 0, 255, cv2.NORM_MINMAX)
-plt.imshow(phase_woman1_normalized)
+r, g, b = cv2.split(woman1)
+
+magnitude, phase = compute_dft_gray(r)
+print(magnitude.shape)
+magnitude = 1000*np.log(magnitude)
+plt.imshow(magnitude)
 
 
-# Reading in image of woman2 and plotting it
-img_woman2 = cv2.imread(path_woman2)
-img_woman2 = cv2.cvtColor(img_woman2, cv2.COLOR_BGR2GRAY)
-plt.imshow(img_woman2)
-plt.axis("off")
+def compute_dft_rgb(img):
+    r, g, b = cv2.split(img)
+    channels = [r, g, b]
+    magnitudes = []
+    phases = []
+    for channel in channels:
+        magnitude, phase = compute_dft_gray(channel)
+        magnitudes.append(magnitude)
+        phases.append(phase)
+    return magnitudes, phases
+
+woman1_magnitude, woman1_phase = compute_dft_rgb(woman1)
+woman2_magnitude, woman2_phase = compute_dft_rgb(woman2)
+
+'''
+woman1_magnitude = 100*np.log(woman1_magnitude[0]).astype(np.uint8)
+#woman1_magnitude = cv2.merge(woman1_magnitude.astype(np.uint8))
+print(woman1_magnitude.shape)
+plt.imshow(woman1_magnitude)
 plt.show()
 
-
-# converting image of woman 2 to frequency domain and plotting magnitude and phase
-dft_woman2 = cv2.dft(np.float32(img_woman2), flags=cv2.DFT_COMPLEX_OUTPUT)
-dft_woman2_shifted = np.fft.fftshift(dft_woman2)
-magnitude_woman2, phase_woman2 = cv2.cartToPolar(dft_woman2_shifted[:,:,0],
-                                                 dft_woman2_shifted[:,:,1])
-magnitude_woman2_normalized = 100*np.log(magnitude_woman2)
-plt.imshow(magnitude_woman2_normalized)
-
-phase_woman2_normalized = cv2.normalize(phase_woman2, None, 0, 255, cv2.NORM_MINMAX)
-plt.imshow(phase_woman2_normalized)
+'''
 
 
-# reconstructing image with magnitude from woman1 and phase from woman2
-real_woman12, imag_woman12 = cv2.polarToCart(magnitude_woman1, phase_woman2)
-dft_woman12 = cv2.merge([real_woman12, imag_woman12])
-dft_woman12_ishift = np.fft.ifftshift(dft_woman12)
-idft_woman12 = cv2.idft(dft_woman12_ishift)
-img_woman12 = cv2.magnitude(idft_woman12[:,:,0], idft_woman12[:,:,1])
-plt.imshow(img_woman12)
-plt.axis("off")
+
+
+def compute_idft_gray(magnitude, phase):
+    real, imag = cv2.polarToCart(magnitude, phase)
+    dft_shift = cv2.merge([real, imag])
+    dft = np.fft.ifftshift(dft_shift)
+    img = cv2.idft(dft)
+    img = cv2.magnitude(img[:,:,0], img[:,:,1])
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
+    #img = np.clip(img, 0, 255).astype(np.uint8)
+    img = (img).astype(np.uint8)
+    return img
+
+def compute_idft_rgb(magnitudes, phases):
+    channels = []
+    for magnitude, phase in zip(magnitudes, phases):
+        channel = compute_idft_gray(magnitude, phase)
+        img = compute_idft_gray(woman2_magnitude[2], woman2_phase[2])
+        channels.append(channel)
+    img = cv2.merge(channels)
+    return img
+
+img = compute_idft_rgb(woman2_magnitude, woman1_phase)
+plt.imshow(img)
 plt.show()
 
-
-# reconstructing image with magnitude from woman2 and phase from woman1
-real_woman21, imag_woman21 = cv2.polarToCart(magnitude_woman2, phase_woman1)
-dft_woman21 = cv2.merge([real_woman21, imag_woman21])
-dft_woman21_ishift = np.fft.ifftshift(dft_woman21)
-idft_woman21 = cv2.idft(dft_woman21_ishift)
-img_woman21 = cv2.magnitude(idft_woman21[:,:,0], idft_woman21[:,:,1])
-plt.imshow(img_woman21)
-plt.axis("off")
+img = compute_idft_rgb(woman1_magnitude, woman2_phase)
+plt.imshow(img)
 plt.show()
-
 
