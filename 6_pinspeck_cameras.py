6951c0c5a206ad1aceec74ca9758ada24f98bae1
Christopher, Malili <malilichristopher@gmail.com>
2025-03-09 06:43:44 -0700

    Portion of video matching outdoor scene isolated

--- 6_pinspeck_cameras.py
+++ 6_pinspeck_cameras.py
@@ -32,44 +32,36 @@ plt.axis("off")
 plt.show()
 
 # Question 6c: Writing out the difference video
-# Convert reference frame to grayscale
-#ref_gray = cv2.cvtColor(ref_frame, cv2.COLOR_RGB2GRAY)
-'''
+
 # Create a VideoWriter to save output
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('livingroom_output.avi', fourcc, 20.0, (ref_frame.shape[1], ref_frame.shape[0]))
-#out = cv2.VideoWriter('living_room_output.avi', fourcc, 20.0, (ref_frame.shape[1], ref_frame.shape[0]))
+out = cv2.VideoWriter('livingroom_output.avi', fourcc, 30.0, (ref_frame.shape[1], ref_frame.shape[0]))
 
+cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+i = 0
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break  # Stop if video ends
 
-    #gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    #gray = cv2.GaussianBlur(gray, (25,25), 0)
-    #gray = cv2.GaussianBlur(gray, (25,25), 0)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_rgb = cv2.GaussianBlur(frame_rgb, (5,5), 0)
     
     # Difference image (Choose reference frame strategy)
-    #diff = cv2.absdiff(ref_gray, gray)  # Simple frame subtraction
-    #diff = cv2.absdiff(ref, frame)  # Simple frame subtraction
-    '''
-'''
-    # Normalize and apply colormap for better visualization
+    diff = cv2.absdiff(ref_frame, frame_rgb)  # Simple frame subtraction
+    
+    # Normalize 
     diff_normalized = cv2.normalize(diff, None, 0, 255, cv2.NORM_MINMAX)
-    diff_colored = cv2.applyColorMap(diff_normalized.astype(np.uint8), cv2.COLORMAP_JET)
-    '''
-    # Show results
-    #cv2.imshow('Difference Video', diff_colored)
-    #out.write(diff_colored)
-'''
-    cv2.imshow('Difference Video', frame)
-    #out.write(diff)
-
+    
+    cv2.imshow('Difference Video', diff_normalized)
+    out.write(diff_normalized)
+    
+    
     # Press 'q' to exit early
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
-'''
+
 # Release resources
 cap.release()
-#out.release()
+out.release()
 cv2.destroyAllWindows()
