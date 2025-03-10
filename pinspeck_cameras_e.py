73f271c3f10121a25b7c280498a2f095ba42be9f
Christopher, Malili <malilichristopher@gmail.com>
2025-03-09 20:39:32 -0700

    Pinspeck problem completed

--- pinspeck_cameras_e.py
+++ pinspeck_cameras_e.py
@@ -6,30 +6,36 @@ Created on Sun Mar  9 06:45:51 2025
 @author: cmalili
 """
 
+import rawpy
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-reference_path = "reference.jpg"
-oclusion_path = "oclusion.jpg"
+#rawpy.libraw.set_maximum_supported_memory_mb  # Set to 8GB (adjust if needed)
 
-reference = cv2.imread(reference_path)
-reference = cv2.cvtColor(reference, cv2.COLOR_BGR2RGB)
 
-oclusion = cv2.imread(oclusion_path)
-oclusion = cv2.cvtColor(oclusion, cv2.COLOR_BGR2RGB)
+#outdoor_path = "outside_scene.dng"
+reference_path = "original_background.dng"
+reference_raw = rawpy.imread(reference_path)
+reference_rgb = reference_raw.postprocess()
 
-diff = (np.absdiff(reference, oclusion)).astype(np.uint8)
+plt.imshow(reference_rgb)
+plt.axis('off')
+plt.show()
+
+oclusion_path = "ocluded_background.dng"
+oclusion_raw = rawpy.imread(oclusion_path)
+oclusion_rgb = oclusion_raw.postprocess()
+
+plt.imshow(oclusion_rgb)
+plt.axis('off')
+plt.show()
+
+
+diff = cv2.absdiff(reference_rgb, oclusion_rgb)#).astype(np.uint8)
 diff = cv2.GaussianBlur(diff, (5,5), 0)
 diff_normalized = cv2.normalize(diff, None, 0, 255, cv2.NORM_MINMAX)
 
-plt.imshow(reference)
-plt.axis('off')
-plt.show()
-
-plt.imshow(oclusion)
-plt.axis('off')
-plt.show()
 
 plt.imshow(diff_normalized)
 plt.axis('off')
